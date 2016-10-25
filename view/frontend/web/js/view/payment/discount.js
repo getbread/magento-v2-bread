5b75f77081f9d2f551855e8fbaac4dcef580e1f1
Miranda Johnson <miranda@mediotype.com>
2016-10-25 10:48:41 -0600

    Invalidate bread token when applying discount at checkout payment step

---
+++ view/frontend/web/js/view/payment/discount.js
@@ -0,0 +1,31 @@
+/**
+ * Clears bread transaction token when changing coupon
+ *
+ * @author  Bread   copyright   2016
+ * @author  Miranda @Mediotype
+ */
+define(
+    [
+        'jquery',
+        'Magento_SalesRule/js/view/payment/discount'
+    ],
+    function ($, Discount) {
+        'use strict';
+        return Discount.extend({
+            /**
+             * Coupon form validation
+             *
+             * @returns {boolean}
+             */
+            validate: function() {
+                if (Discount.prototype.validate.call(this)) { // Call parent method
+                    if (window.checkoutConfig.payment.breadcheckout.transactionId !== null) {
+                        window.checkoutConfig.payment.breadcheckout.transactionId = null;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+);
