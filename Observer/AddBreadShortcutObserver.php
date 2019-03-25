97b34580d7065e2e5d98c63cd276cf1cbd2c1185
Aleksa Zivkovic <aleksa@andersinnovations.com>
2019-03-25 12:01:42 +0100

    BF-162 add bread button to mini cart

---
+++ Observer/AddBreadShortcutObserver.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bread\BreadCheckout\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+
+/**
+ * Class AddBreadShortcutObserver
+ * @package Bread\BreadCheckout\Observer
+ */
+class AddBreadShortcutObserver implements ObserverInterface
+{
+
+    const BREAD_SHORTCUT_BLOCK = \Bread\BreadCheckout\Block\Checkout\Minicart::class;
+
+    /**
+     * @param Observer $observer
+     */
+    public function execute(Observer $observer)
+    {
+
+        if ($observer->getData('is_catalog_product')) {
+            return;
+        }
+
+        $shortcutButtons = $observer->getEvent()->getContainer();
+
+        $shortcut = $shortcutButtons->getLayout()->createBlock(self::BREAD_SHORTCUT_BLOCK);
+
+        $shortcutButtons->addShortcut($shortcut);
+    }
+}
